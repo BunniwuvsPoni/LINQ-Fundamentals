07a13ca992a37bd1bddf2de0da728d0679b5f3c4
Tony Wang <zichutonywang@outlook.com>
2019-09-09 16:07:43 -0400

    Rewrote LINQ using slightly different syntax.

--- ConsoleApp_Directory/Program.cs
+++ ConsoleApp_Directory/Program.cs
@@ -18,10 +18,11 @@ namespace ConsoleApp_Directory
 
         private static void ShowLargeFilesWithLINQ(string path)
         {
-            var query = from file in new DirectoryInfo(path).GetFiles()
-                        orderby file.Length descending
-                        select file;
-            foreach(var file in query.Take(5))
+            var query = new DirectoryInfo(path).GetFiles()
+                            .OrderByDescending(f => f.Length)
+                            .Take(5);
+
+            foreach(var file in query)
             {
                 Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
             }
