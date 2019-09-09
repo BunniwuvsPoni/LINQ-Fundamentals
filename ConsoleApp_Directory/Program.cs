f22a2d43e75790c8e21c0a4024eacad2d9a4c012
Tony Wang <zichutonywang@outlook.com>
2019-09-09 16:01:08 -0400

    Added Show Large Files (top 5) without LINQ.

--- ConsoleApp_Directory/Program.cs
+++ ConsoleApp_Directory/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleApp_Directory
 {
@@ -7,6 +9,28 @@ namespace ConsoleApp_Directory
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string path = @"C:\Windows";
+            ShowLargeFilesWithoutLINQ(path);
+        }
+
+        private static void ShowLargeFilesWithoutLINQ(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);  //  Creates new directory object to pull file information
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());  //  Sorts returned files
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo> //  Old way of doing comparison
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
