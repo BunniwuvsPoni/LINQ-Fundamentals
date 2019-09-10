c86a8fb8b8ee90e671b5f079780f7b80101ba420
Tony Wang <zichutonywang@outlook.com>
2019-09-10 14:30:15 -0400

    New queries project.

---
+++ ConsoleApp_Queries/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp_Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var movies = new List<Movie>
+            {
+                new Movie {Title = "The Dark Knight", Rating = 8.9f, Year = 2013},
+                new Movie {Title = "The King's Speech", Rating = 8.0f, Year = 2017},
+                new Movie {Title = "Casablanca", Rating = 8.5f, Year = 1985},
+                new Movie {Title = "Star Wars V", Rating = 8.7f, Year = 1956}
+            };
+        }
+    }
+}
