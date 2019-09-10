b3ef78adc66222ece7111e049a8b7342a87200fd
Tony Wang <zichutonywang@outlook.com>
2019-09-10 11:44:21 -0400

    Features test.

---
+++ ConsoleApp_Features/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp_Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Employee[] developers = new Employee[]
+            {
+                new Employee { Id = 1, Name="Scott" },
+                new Employee { Id = 2, Name="Chris" }
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name="Alex" }
+            };
+        }
+    }
+}
